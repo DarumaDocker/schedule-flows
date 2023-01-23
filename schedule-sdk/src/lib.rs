534f983f1eb7553f3527ab6aabbe28e7be30e8c5
DarumaDocker <DarumaDocker@gmail.com>
2023-01-23 16:46:27 +0800

    update docs

--- schedule-sdk/src/lib.rs
+++ schedule-sdk/src/lib.rs
@@ -1,3 +1,27 @@
+//! Make flow function acting as scheduled task in [Flows.network](https://test.flows.network)
+//!
+//! # Quick Start
+//!
+//! To get started, let's write a task that will send a message to Slack at a fixed time wach day.
+//!
+//! ```
+//! use schedule_flows::cron_job_evoked;
+//! use slack_flows::send_message_to_channel;
+//!
+//! #[no_mangle]
+//! pub fn run() {
+//!     if let Some(body) = cron_job_evoked(String::from("50 8 * * *"), String::from("cron_job_evoked")) {
+//!         send_message_to_channel(
+//!             "myworkspace",
+//!             "mychannel",
+//!             String::from_utf8_lossy(&body).into_owned(),
+//!         );
+//!     }
+//! }
+//! ```
+//!
+//! [cron_job_evoked] will create a cron job and return the content
+//! when the job is triggered at 8:50 UTC each day.
 use http_req::request;
 
 const SCHEDULE_API_PREFIX: &str = "https://schedule-flows-extension.vercel.app/api";
@@ -11,6 +35,15 @@ extern "C" {
     fn set_error_log(p: *const u8, len: i32);
 }
 
+/// Create a cron job and return the content when the job is triggered.
+///
+/// `cron` is a [cron expression](https://crontab.guru/). There is
+/// currently a limitation to use this function. The minute and hour
+/// should be specified as exact number. That's to say, you can not
+/// set `*`, value list or range of values to these two fields.
+///
+/// The return bytes vec is currently the same as the body passed to
+/// the function.
 pub fn cron_job_evoked(cron: String, body: String) -> Option<Vec<u8>> {
     unsafe {
         match is_listening() {
